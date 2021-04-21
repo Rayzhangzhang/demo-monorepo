553805e417f58fee29e2b476c5a34a4b24a94289
Rayzhangzhang <16114124@hdu.edu.cn>
2021-04-21 19:42:34 +0800

    feat: init demo

--- packages/a-package/src/index.ts
+++ packages/a-package/src/index.ts
@@ -2,9 +2,10 @@ import { get } from 'lodash'
 
 import MD from 'react-markdown-editor-lite'
 
-export const MDi = MD
-
-export default () => {
-  console.log(get)
-  console.log(12)
+export default {
+  fuc: () => {
+    console.log(get)
+    console.log(12)
+  },
+  MD
 }
