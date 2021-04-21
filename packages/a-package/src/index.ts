8624709c04afb92fd1732df97ff74b0b90f4647e
Rayzhangzhang <16114124@hdu.edu.cn>
2021-04-21 16:52:33 +0800

    feat: init

--- packages/a-package/src/index.ts
+++ packages/a-package/src/index.ts
@@ -1,5 +1,9 @@
 import { get } from 'lodash'
 
+import MD from 'react-markdown-editor-lite'
+
+export const MDi = MD
+
 export default () => {
   console.log(get)
   console.log(12)
--- packages/b-package/src/index.ts
+++ packages/b-package/src/index.ts
@@ -1,3 +1,5 @@
 import A from "a-package-zpf-test";
+import MD from 'react-markdown-editor-lite'
 
+export const MDi = MD
 export default A
